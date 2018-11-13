69796048c97a21663b7e8179813049299ec92a20
Naruse <schoolnarunaru@gmail.com>
2018-11-13 13:36:51 +0900

    扱うデータの変更とそれに伴った判断式の変更

--- GeneticAlgorithms/Main.cpp
+++ GeneticAlgorithms/Main.cpp
@@ -6,8 +6,9 @@
 #include	<time.h>
 
 //---- 定数定義 ----
-#define		BAG_SIZE	400
-#define		ITEM_NUM	20
+#define		BAG_SIZE	400				// バッグの許容重量
+#define		BAG_CAPA	10				// バッグの積載量
+#define		ITEM_NUM	64
 #define		POP_SIZE	10				// 遺伝子総数
 #define		GENE_LENGTH	(ITEM_NUM)		// 遺伝子のビット数
 
@@ -16,9 +17,35 @@
 
 #define		GENE_MAX	10				// 世代交代数
 
+const float	g_DataVolume[] = { 
+  0.072431f, 0.470347f, 0.064719f, 0.285837f, 0.225093f, 0.096393f, 0.515726f, 0.455327f,
+  0.165786f, 0.167166f, 0.536347f, 0.147685f, 0.091873f, 0.496223f, 0.253056f, 0.516451f,
+  0.067792f, 0.544281f, 0.523754f, 0.277838f, 0.078748f, 0.167894f, 0.302647f, 0.199729f,
+  0.398927f, 0.366057f, 0.500194f, 0.192629f,  0.07454f,   0.1858f, 0.288475f, 0.054764f,
+  0.178058f, 0.543278f, 0.082055f, 0.147755f, 0.056136f, 0.377898f, 0.149305f, 0.172916f,
+  0.547585f, 0.379011f, 0.504197f, 0.078298f, 0.329076f, 0.494272f, 0.528666f, 0.125196f,
+  0.515643f, 0.516848f, 0.200623f, 0.470438f, 0.074357f, 0.272055f, 0.421436f, 0.058918f,
+  0.530786f, 0.327218f, 0.186787f, 0.488957f, 0.257442f, 0.326924f, 0.143436f, 0.107303f };
 
-const int g_DataWeight[] = { 80,70,60,34,89,10,20,30,90,38,75,24,55,15,28,40,50,64,15,79 };
-const int g_DataValue[] = { 10, 5, 20,8,2,5,7,14,22,9,11,3,18,13,6,21,24,1,4,16 };
+const float g_DataWeight[] = { 
+  1.042288f, 1.096132f, 0.952216f,  0.79302f, 1.046246f, 0.823767f, 0.873065f, 0.758806f,
+  0.737015f, 1.028084f, 0.606415f, 0.881106f, 0.884353f, 1.290026f, 0.774216f, 1.014714f,
+  1.229122f, 1.015562f, 0.964802f, 1.288146f, 1.076072f, 0.952694f, 0.704871f, 0.904247f,
+  0.846266f, 0.837016f, 1.059204f, 0.699134f, 0.870344f, 0.858246f, 0.743155f, 0.800332f,
+  1.160441f, 0.715929f, 1.206835f, 1.056951f, 0.898189f, 1.226303f, 1.025044f, 0.640907f,
+  0.899368f, 1.225172f, 0.901907f, 1.068226f, 0.712972f, 1.055511f, 1.114367f, 0.632971f,
+  1.235422f, 0.818592f, 1.277626f, 0.976713f, 0.825303f, 0.786353f, 0.904311f, 1.125975f,
+	0.9002f, 0.687584f, 1.253057f, 0.609091f,  0.88333f,  0.89934f, 0.928529f, 1.195918f };
+
+const int g_DataValue[] = {
+  515, 802, 778, 406, 146, 914, 929, 317,
+  556, 788, 438, 897, 127, 425, 909, 650,
+  802, 532, 768, 524, 376, 204, 113, 627,
+  256, 869, 488, 932, 229, 418, 147, 234,
+  210, 290, 161, 655, 820, 981, 835, 903,
+  864, 468, 908, 421, 128, 446, 298, 289,
+  762, 358, 165, 636, 631, 781, 196, 421,
+  446, 402, 431, 205, 511, 441, 680, 688 };
 
 
 //---- グローバル変数 ----
@@ -42,7 +69,8 @@ int calc_fitness_gene(int gene_no);
 //---- エントリポイント ---- 
 int main()
 {
-	int	i, j, wk_weight;
+	int		i, j;
+	float	wk_weight, wk_volume;
 
 	// 初期化
 	i = 0;
@@ -72,22 +100,24 @@ int main()
 
 	// 結果を出力
 	printf("==== 計算終了 ====\n");
-	printf("最高適応度		= %3d\n", g_AllMaxFitness);
-	printf("最高遺伝子世代	= %3d\n", g_AllMaxFitnessGeneration);
-	printf("最高遺伝子[%2d]	= ", g_AllMaxFitnessIndex);
+	printf("最高適応度       = %3d\n", g_AllMaxFitness);
+	printf("最高遺伝子世代   = %3d\n", g_AllMaxFitnessGeneration);
+	printf("最高遺伝子[%2d]   = ", g_AllMaxFitnessIndex);
 
 	wk_weight = 0;
+	wk_volume = 0;
 	for (i = 0; i < GENE_LENGTH; i++)
 	{
-		printf(" %1d", g_AllMaxFitnessGene[i]);
+		printf("%1d", g_AllMaxFitnessGene[i]);
 		if (g_AllMaxFitnessGene[i] == 1)
 		{
 			wk_weight += g_DataWeight[i];
+			wk_volume += g_DataVolume[i];
 		}
 	}
 	printf("\n");
-	printf("総重量 = %d\n", wk_weight);
-
+	printf("総重量 = %f\n", wk_weight);
+	printf("総体積 = %f\n", wk_volume);
 
 	// 終了処理
 	printf("Push Enter-key >> ");
@@ -261,7 +291,7 @@ void calc_fitness_pop()
 		printf("遺伝子[%2d] = ", g_AllMaxFitnessIndex);
 		for (i = 0; i < GENE_LENGTH; ++i)
 		{
-			printf("%2d", g_AllMaxFitnessGene[i]);
+			printf("%d", g_AllMaxFitnessGene[i]);
 		}
 		printf("\n");
 		printf("適応度 = %d\n", g_AllMaxFitness);
@@ -270,26 +300,31 @@ void calc_fitness_pop()
 
 int calc_fitness_gene(int gene_no)
 {
-	int i, work_w, work_v;
-	work_w = 0;
-	work_v = 0;
+	int i, work_value;
+	float work_weight, work_volume;
+
+	work_weight = 0;
+	work_value = 0;
+	work_volume = 0;
 
 	for (i = 0; i < GENE_LENGTH; i++)
 	{
 		// 遺伝子情報が「１」なら「重さ」「価値」を加算する
 		if (g_Gene[gene_no][i] == 1)
 		{
-			work_w += g_DataWeight[i];
-			work_v += g_DataValue[i];
+			work_weight += g_DataWeight[i];
+			work_volume += g_DataVolume[i];
+			work_value += g_DataValue[i];
 		}
 	}
 
 	// もし合計値がバッグの許容量をオーバーしたら適応度０とする
-	if (work_w > BAG_SIZE)
+	if (work_weight > BAG_SIZE || 
+		work_volume > BAG_CAPA)
 	{
-		work_v = 0;
+		work_value = 0;
 	}
-	return work_v;
+	return work_value;
 }
 
 void check_result()
@@ -302,7 +337,7 @@ void check_result()
 		
 		for (j = 0; j < GENE_LENGTH; j++)
 		{
-			printf(" %1d", g_Gene[i][j]);
+			printf("%1d", g_Gene[i][j]);
 		}
 		printf("\n");
 	}
